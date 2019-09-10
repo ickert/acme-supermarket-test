b20684859f2ce70c4f4453766fa90f24b4095ec9
Felipe Ickert <fvickert@gmail.com>
2019-09-10 23:35:10 +0200

    added data of products as mocked information

---
+++ src/mockData/products.js
@@ -0,0 +1,19 @@
+const products = [
+    {
+        key: `FR1`,
+        name: `Fruit tea`,
+        price: 3.11
+    },
+    {
+        key: `SR1`,
+        name: `Strawberries`,
+        price: 5.00
+    },
+    {
+        key: `CF1`,
+        name: `Coffee`,
+        price: 11.23
+    }
+];
+
+export default products;
